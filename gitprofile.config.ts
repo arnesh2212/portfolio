28fa74d9b89c928dbcd4e3ec28a4482b5e97a79d
Arnesh Batra <66691657+arnesh2212@users.noreply.github.com>
2024-12-24 19:59:29 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -162,7 +162,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'sunset',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
